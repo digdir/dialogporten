ce5357b9a66057a9a13bed7e7d4329fcb3258fb9
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2023-06-27 09:44:26 +0200

    Fix type mapping for incoming requests. (#93)

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/Dialogs/Commands/Create/MappingProfile.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/Dialogs/Commands/Create/MappingProfile.cs
@@ -11,16 +11,21 @@ internal sealed class MappingProfile : Profile
 	public MappingProfile()
 	{
 		CreateMap<CreateDialogDto, DialogEntity>()
+			.ForMember(dest => dest.Status, opt => opt.Ignore())
             .ForMember(dest => dest.StatusId, opt => opt.MapFrom(src => src.Status));
         CreateMap<CreateDialogDialogElementDto, DialogElement>();
-        CreateMap<CreateDialogDialogElementUrlDto, DialogElementUrl>()
+		CreateMap<CreateDialogDialogElementUrlDto, DialogElementUrl>()
+			.ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
 			.ForMember(dest => dest.ConsumerTypeId, opt => opt.MapFrom(src => src.ConsumerType));
 		CreateMap<CreateDialogDialogGuiActionDto, DialogGuiAction>()
+			.ForMember(dest => dest.Priority, opt => opt.Ignore())
             .ForMember(dest => dest.PriorityId, opt => opt.MapFrom(src => src.Priority));
 		CreateMap<CreateDialogDialogApiActionDto, DialogApiAction>();
-        CreateMap<CreateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
+		CreateMap<CreateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
+			.ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
 			.ForMember(dest => dest.HttpMethodId, opt => opt.MapFrom(src => src.HttpMethod));
 		CreateMap<CreateDialogDialogActivityDto, DialogActivity>()
+			.ForMember(dest => dest.Type, opt => opt.Ignore())
             .ForMember(dest => dest.TypeId, opt => opt.MapFrom(src => src.Type));
     }
 }
--- src/Digdir.Domain.Dialogporten.Application/Features/V1/Dialogs/Commands/Update/MappingProfile.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/Dialogs/Commands/Update/MappingProfile.cs
@@ -16,6 +16,7 @@ internal sealed class MappingProfile : Profile
         // In
         CreateMap<UpdateDialogDto, DialogEntity>()
             .IgnoreComplexDestinationProperties()
+			.ForMember(dest => dest.Status, opt => opt.Ignore())
             .ForMember(dest => dest.StatusId, opt => opt.MapFrom(src => src.Status));
 
         CreateMap<UpdateDialogDialogApiActionDto, DialogApiAction>()
@@ -24,11 +25,13 @@ internal sealed class MappingProfile : Profile
         CreateMap<UpdateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
+			.ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
             .ForMember(dest => dest.HttpMethodId, opt => opt.MapFrom(src => src.HttpMethod));
 
         CreateMap<UpdateDialogDialogGuiActionDto, DialogGuiAction>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
+			.ForMember(dest => dest.Priority, opt => opt.Ignore())
             .ForMember(dest => dest.PriorityId, opt => opt.MapFrom(src => src.Priority));
 
         CreateMap<UpdateDialogDialogElementDto, DialogElement>()
@@ -36,11 +39,13 @@ internal sealed class MappingProfile : Profile
         CreateMap<UpdateDialogDialogElementUrlDto, DialogElementUrl>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
+			.ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
             .ForMember(dest => dest.ConsumerTypeId, opt => opt.MapFrom(src => src.ConsumerType));
 
         // Since this is append only, we don't need to merge with existing
         // activity records and thus can map complex properties
         CreateMap<UpdateDialogDialogActivityDto, DialogActivity>()
+			.ForMember(dest => dest.Type, opt => opt.Ignore())
             .ForMember(dest => dest.TypeId, opt => opt.MapFrom(src => src.Type));
 
         // To support json patch
