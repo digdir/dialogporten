504c7fbe5d46aad41d80f5b44dfbf52f4f818c05
Ole Jørgen Skogstad <skogstad@softis.net>
2023-08-24 11:18:35 +0200

    Add GET dialog elements endpoints for service owners

---
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/DialogElements/Queries/Get/MappingProfile.cs
@@ -0,0 +1,16 @@
+using AutoMapper;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.DialogElements;
+
+namespace Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.DialogElements.Queries.Get;
+
+public class MappingProfile : Profile
+{
+    public MappingProfile()
+    {
+        CreateMap<DialogElement, GetDialogElementDto>()
+            .ForMember(dest => dest.DeletedAt, opt => opt.MapFrom(src => src.Dialog.DeletedAt));
+
+        CreateMap<DialogElementUrl, GetDialogElementUrlDto>()
+            .ForMember(dest => dest.ConsumerType, opt => opt.MapFrom(src => src.ConsumerTypeId));
+    }
+}
