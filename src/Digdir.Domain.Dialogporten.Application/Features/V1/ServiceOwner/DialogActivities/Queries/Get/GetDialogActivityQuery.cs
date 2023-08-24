679e7b5823eb795c74ca0e1f3393aa703ac18aa8
Ole Jørgen Skogstad <skogstad@softis.net>
2023-08-24 08:46:38 +0200

    Add GET dialog activities for service owners (#146)
    * Add GET dialog activities for service owners
    
    * Remove EntityDeleted
    
    Co-authored-by: Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
    
    * Rename DeletedAt property
    
    ---------
    
    Co-authored-by: Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>

---
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/DialogActivities/Queries/Get/GetDialogActivityQuery.cs
@@ -0,0 +1,57 @@
+using AutoMapper;
+using Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
+using Digdir.Domain.Dialogporten.Application.Externals;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Activities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using OneOf;
+
+namespace Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.DialogActivities.Queries.Get;
+
+public sealed class GetDialogActivityQuery : IRequest<GetDialogActivityResult>
+{
+    public Guid DialogId { get; set; }
+    public Guid ActivityId { get; set; }
+}
+
+[GenerateOneOf]
+public partial class GetDialogActivityResult : OneOfBase<GetDialogActivityDto, EntityNotFound>
+{
+}
+
+internal sealed class GetDialogActivityQueryHandler : IRequestHandler<GetDialogActivityQuery, GetDialogActivityResult>
+{
+    private readonly IMapper _mapper;
+    private readonly IDialogDbContext _dbContext;
+
+    public GetDialogActivityQueryHandler(IMapper mapper, IDialogDbContext dbContext)
+    {
+        _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+    }
+
+    public async Task<GetDialogActivityResult> Handle(GetDialogActivityQuery request,
+        CancellationToken cancellationToken)
+    {
+        var dialog = await _dbContext.Dialogs
+            .Include(x => x.Activities.Where(x => x.Id == request.ActivityId))
+            .IgnoreQueryFilters()
+            .FirstOrDefaultAsync(x => x.Id == request.DialogId,
+                cancellationToken: cancellationToken);
+
+        if (dialog is null)
+        {
+            return new EntityNotFound<DialogEntity>(request.DialogId);
+        }
+
+        var activity = dialog.Activities.FirstOrDefault();
+
+        if (activity is null)
+        {
+            return new EntityNotFound<DialogActivity>(request.ActivityId);
+        }
+
+        return _mapper.Map<GetDialogActivityDto>(activity);
+    }
+}
---
+++ src/Digdir.Domain.Dialogporten.WebApi/Endpoints/V1/ServiceOwner/DialogActivity/GetDialogActivityEndpoint.cs
@@ -0,0 +1,29 @@
+using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.DialogActivities.Queries.Get;
+using FastEndpoints;
+using MediatR;
+
+namespace Digdir.Domain.Dialogporten.WebApi.Endpoints.V1.ServiceOwner.DialogActivity;
+
+public class GetDialogActivityEndpoint : Endpoint<GetDialogActivityQuery>
+{
+    private readonly ISender _sender;
+
+    public GetDialogActivityEndpoint(ISender sender)
+    {
+        _sender = sender ?? throw new ArgumentNullException(nameof(sender));
+    }
+
+    public override void Configure()
+    {
+        Get("dialogs/{dialogId}/activities/{activityId}");
+        Group<ServiceOwnerGroup>();
+    }
+
+    public override async Task HandleAsync(GetDialogActivityQuery req, CancellationToken ct)
+    {
+        var result = await _sender.Send(req, ct);
+        await result.Match(
+            dto => SendOkAsync(dto, ct),
+            notFound => this.NotFoundAsync(notFound, ct));
+    }
+}
