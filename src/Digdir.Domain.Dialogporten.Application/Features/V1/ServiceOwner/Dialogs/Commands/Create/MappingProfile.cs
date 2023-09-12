7ca9e426dc0fe6854c7b32ebaaaab6a6778a07d2
Ole Jørgen Skogstad <skogstad@softis.net>
2023-09-12 13:33:51 +0200

    Fix indentation

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Create/MappingProfile.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Create/MappingProfile.cs
@@ -8,26 +8,32 @@ namespace Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialog
 
 internal sealed class MappingProfile : Profile
 {
-	public MappingProfile()
-	{
-		CreateMap<CreateDialogDto, DialogEntity>()
-			.ForMember(dest => dest.Status, opt => opt.Ignore())
+    public MappingProfile()
+    {
+        CreateMap<CreateDialogDto, DialogEntity>()
+            .ForMember(dest => dest.Status, opt => opt.Ignore())
             .ForMember(dest => dest.StatusId, opt => opt.MapFrom(src => src.Status));
+        
         CreateMap<CreateDialogDialogElementDto, DialogElement>();
+        
         CreateMap<CreateDialogSearchTagDto, DialogSearchTag>();
-		CreateMap<CreateDialogDialogElementUrlDto, DialogElementUrl>()
-			.ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
-			.ForMember(dest => dest.ConsumerTypeId, opt => opt.MapFrom(src => src.ConsumerType));
-		CreateMap<CreateDialogDialogGuiActionDto, DialogGuiAction>()
-			.ForMember(dest => dest.Priority, opt => opt.Ignore())
+        
+        CreateMap<CreateDialogDialogElementUrlDto, DialogElementUrl>()
+            .ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
+            .ForMember(dest => dest.ConsumerTypeId, opt => opt.MapFrom(src => src.ConsumerType));
+        
+        CreateMap<CreateDialogDialogGuiActionDto, DialogGuiAction>()
+            .ForMember(dest => dest.Priority, opt => opt.Ignore())
             .ForMember(dest => dest.PriorityId, opt => opt.MapFrom(src => src.Priority));
-		CreateMap<CreateDialogDialogApiActionDto, DialogApiAction>();
-		CreateMap<CreateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
-			.ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
-			.ForMember(dest => dest.HttpMethodId, opt => opt.MapFrom(src => src.HttpMethod));
-		CreateMap<CreateDialogDialogActivityDto, DialogActivity>()
-			.ForMember(dest => dest.Type, opt => opt.Ignore())
+        
+        CreateMap<CreateDialogDialogApiActionDto, DialogApiAction>();
+        
+        CreateMap<CreateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
+            .ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
+            .ForMember(dest => dest.HttpMethodId, opt => opt.MapFrom(src => src.HttpMethod));
+        
+        CreateMap<CreateDialogDialogActivityDto, DialogActivity>()
+            .ForMember(dest => dest.Type, opt => opt.Ignore())
             .ForMember(dest => dest.TypeId, opt => opt.MapFrom(src => src.Type));
     }
 }
-
