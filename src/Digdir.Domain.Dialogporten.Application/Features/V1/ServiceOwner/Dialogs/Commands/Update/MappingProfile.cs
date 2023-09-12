aafbb4b8cc50aa597d870aea3abec2e1da3dc55e
Ole Jørgen Skogstad <skogstad@softis.net>
2023-09-12 13:34:59 +0200

    Fix indentation

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Update/MappingProfile.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Update/MappingProfile.cs
@@ -15,42 +15,42 @@ internal sealed class MappingProfile : Profile
         // In
         CreateMap<UpdateDialogDto, DialogEntity>()
             .IgnoreComplexDestinationProperties()
-			.ForMember(dest => dest.Status, opt => opt.Ignore())
+            .ForMember(dest => dest.Status, opt => opt.Ignore())
             .ForMember(dest => dest.StatusId, opt => opt.MapFrom(src => src.Status));
 
         CreateMap<UpdateDialogSearchTagDto, DialogSearchTag>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore());
-        
+
         CreateMap<UpdateDialogDialogApiActionDto, DialogApiAction>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore());
-        
+
         CreateMap<UpdateDialogDialogApiActionEndpointDto, DialogApiActionEndpoint>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
-			.ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
+            .ForMember(dest => dest.HttpMethod, opt => opt.Ignore())
             .ForMember(dest => dest.HttpMethodId, opt => opt.MapFrom(src => src.HttpMethod));
 
         CreateMap<UpdateDialogDialogGuiActionDto, DialogGuiAction>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
-			.ForMember(dest => dest.Priority, opt => opt.Ignore())
+            .ForMember(dest => dest.Priority, opt => opt.Ignore())
             .ForMember(dest => dest.PriorityId, opt => opt.MapFrom(src => src.Priority));
 
         CreateMap<UpdateDialogDialogElementDto, DialogElement>()
             .IgnoreComplexDestinationProperties();
-        
+
         CreateMap<UpdateDialogDialogElementUrlDto, DialogElementUrl>()
             .IgnoreComplexDestinationProperties()
             .ForMember(x => x.Id, opt => opt.Ignore())
-			.ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
+            .ForMember(dest => dest.ConsumerType, opt => opt.Ignore())
             .ForMember(dest => dest.ConsumerTypeId, opt => opt.MapFrom(src => src.ConsumerType));
 
         // Since this is append only, we don't need to merge with existing
         // activity records and thus can map complex properties
         CreateMap<UpdateDialogDialogActivityDto, DialogActivity>()
-			.ForMember(dest => dest.Type, opt => opt.Ignore())
+            .ForMember(dest => dest.Type, opt => opt.Ignore())
             .ForMember(dest => dest.TypeId, opt => opt.MapFrom(src => src.Type));
 
         // To support json patch
