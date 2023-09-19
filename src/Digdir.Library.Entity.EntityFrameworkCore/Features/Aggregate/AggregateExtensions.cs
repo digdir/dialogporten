d520e5fbaa644143f2402451821ed410660acf2f
Ole Jørgen Skogstad <skogstad@softis.net>
2023-09-19 13:55:46 +0200

    Checkpoint

--- src/Digdir.Library.Entity.EntityFrameworkCore/Features/Aggregate/AggregateExtensions.cs
+++ src/Digdir.Library.Entity.EntityFrameworkCore/Features/Aggregate/AggregateExtensions.cs
@@ -81,78 +81,24 @@ public static class AggregateExtensions
         this IEnumerable<EntityEntry> entries,
         CancellationToken cancellationToken)
     {
-        var childrenByParent = new Dictionary<EntityEntry, HashSet<EntityEntry>>(comparer: _entityEntryComparer);
+        var childrenByParent = new Dictionary<EntityEntry, AggregateNode>(comparer: _entityEntryComparer);
 
         foreach (var entry in entries)
         {
             await childrenByParent.AddAggregateParentChain(entry, cancellationToken);
         }
 
-        return childrenByParent.ToAggregateNodeByEntry();
-    }
-
-    private static async Task AddAggregateParentChain2(
-        this Dictionary<EntityEntry, AggregateNode> childrenByParent,
-        EntityEntry entry,
-        CancellationToken cancellationToken)
-    {
-        if (!childrenByParent.ContainsKey(entry))
-        {
-            childrenByParent[entry] = new AggregateNode(entry.Entity);
-        }
-
-        foreach (var parentForeignKey in entry.Metadata.FindAggregateParents())
-        {
-            // Supports only dependent to principal. That is - one-to-one and one-to-many
-            // relationships. Many-to-many relationships is not supported.
-            var parentType = parentForeignKey.PrincipalEntityType.ClrType;
-
-            var parentPrimaryKey = parentForeignKey
-                .Properties
-                .Select(key => entry.OriginalValues[key.Name])
-                .ToArray();
-
-            if (parentPrimaryKey.All(x => x is null))
-            {
-                continue;
-            }
-
-            var parentEntity =
-                await entry.Context.FindAsync(parentType, parentPrimaryKey, cancellationToken: cancellationToken);
-
-            if (parentEntity is null)
-            {
-                continue;
-            }
-
-            var parentEntry = entry.Context.Entry(parentEntity);
-
-            // If the parent is known to the dictionary, then we have
-            // already traversed its parent chain from a previous
-            // child. We need not traverse it again. Add the current
-            // entry as a child and continue.
-            if (childrenByParent.TryGetValue(parentEntry, out var children))
-            {
-                children.AddChild(new AggregateNode(entry.Entity));
-                continue;
-            }
-
-            // The parent is unknown to the dictionary, so we add it
-            // with the current entry as a child and traverse its
-            // parent chain.
-            childrenByParent[parentEntry] = new AggregateNode(parentEntry.Entity);
-            await childrenByParent.AddAggregateParentChain2(parentEntry, cancellationToken);
-        }
+        return childrenByParent;
     }
 
     private static async Task AddAggregateParentChain(
-        this Dictionary<EntityEntry, HashSet<EntityEntry>> childrenByParent,
+        this Dictionary<EntityEntry, AggregateNode> nodeByEntry,
         EntityEntry entry,
         CancellationToken cancellationToken)
     {
-        if (!childrenByParent.ContainsKey(entry))
+        if (!nodeByEntry.ContainsKey(entry))
         {
-            childrenByParent[entry] = new HashSet<EntityEntry>(comparer: _entityEntryComparer);
+            nodeByEntry[entry] = new AggregateNode(entry.Entity);
         }
 
         foreach (var parentForeignKey in entry.Metadata.FindAggregateParents())
@@ -185,58 +131,18 @@ public static class AggregateExtensions
             // already traversed its parent chain from a previous
             // child. We need not traverse it again. Add the current
             // entry as a child and continue.
-            if (childrenByParent.TryGetValue(parentEntry, out var children))
+            if (nodeByEntry.TryGetValue(parentEntry, out var parentNode))
             {
-                children.Add(entry);
+                parentNode.AddChild(nodeByEntry[entry]);
                 continue;
             }
 
             // The parent is unknown to the dictionary, so we add it
             // with the current entry as a child and traverse its
             // parent chain.
-            childrenByParent[parentEntry] = new HashSet<EntityEntry>(new[] {entry}, comparer: _entityEntryComparer);
-            await childrenByParent.AddAggregateParentChain(parentEntry, cancellationToken);
-        }
-    }
-
-    private static Dictionary<EntityEntry, AggregateNode> ToAggregateNodeByEntry(
-        this Dictionary<EntityEntry, HashSet<EntityEntry>> childrenByParent)
-    {
-        var nodeByEntry = new Dictionary<EntityEntry, AggregateNode>(comparer: _entityEntryComparer);
-
-        foreach (var (parent, children) in childrenByParent)
-        {
-            // If the parent is known by the dictionary then its been
-            // added and expanded as part of another parent chain.
-            if (nodeByEntry.ContainsKey(parent))
-            {
-                continue;
-            }
-
-            nodeByEntry[parent] = new AggregateNode(parent.Entity);
-            nodeByEntry[parent].ExpandChildren(children, childrenByParent, nodeByEntry);
-        }
-
-        return nodeByEntry;
-    }
-
-    private static void ExpandChildren(
-        this AggregateNode parentNode,
-        IEnumerable<EntityEntry> children,
-        Dictionary<EntityEntry, HashSet<EntityEntry>> childrenByParent,
-        Dictionary<EntityEntry, AggregateNode> nodeByEntry)
-    {
-        foreach (var child in children)
-        {
-            // If the child is not known by the dictionary then we need
-            // to create the child node and expand its children.
-            if (!nodeByEntry.TryGetValue(child, out var expandedChild))
-            {
-                nodeByEntry[child] = expandedChild = new AggregateNode(child.Entity);
-                nodeByEntry[child].ExpandChildren(childrenByParent[child], childrenByParent, nodeByEntry);
-            }
-
-            parentNode.AddChild(expandedChild);
+            nodeByEntry[parentEntry] = new AggregateNode(parentEntry.Entity);
+            nodeByEntry[parentEntry].AddChild(nodeByEntry[entry]);
+            await nodeByEntry.AddAggregateParentChain(parentEntry, cancellationToken);
         }
     }
 
