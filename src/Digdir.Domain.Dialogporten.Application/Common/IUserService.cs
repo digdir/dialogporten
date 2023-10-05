53f82dd056be6aa125ab282072af37923cf6d9c2
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2023-10-05 14:15:26 +0200

    Minor adjustments

--- src/Digdir.Domain.Dialogporten.Application/Common/IUserService.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/IUserService.cs
@@ -1,5 +1,4 @@
-﻿using Digdir.Domain.Dialogporten.Application.Common;
-using Digdir.Domain.Dialogporten.Application.Externals.Presentation;
+﻿using Digdir.Domain.Dialogporten.Application.Externals.Presentation;
 using Digdir.Domain.Dialogporten.Application.Externals;
 using System.Diagnostics;
 using Digdir.Domain.Dialogporten.Application.Common.Extensions;
@@ -14,46 +13,46 @@ internal interface IUserService
 
 internal sealed class UserService : IUserService
 {
-private readonly IUser _user;
-private readonly IResourceRegistry _resourceRegistry;
+    private readonly IUser _user;
+    private readonly IResourceRegistry _resourceRegistry;
 
-public UserService(
-    IUser user,
-    IResourceRegistry resourceRegistry)
-{
-    _user = user ?? throw new ArgumentNullException(nameof(user));
-    _resourceRegistry = resourceRegistry ?? throw new ArgumentNullException(nameof(resourceRegistry));
-}
-
-public async Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken)
-{
-    var resourceIds = await GetCurrentUserResourceIds(cancellationToken);
-    return resourceIds.Contains(serviceResource);
-}
-
-public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken)
-{
-    if (!_user.TryGetOrgNumber(out var orgNumber))
+    public UserService(
+        IUser user,
+        IResourceRegistry resourceRegistry)
     {
-        throw new UnreachableException();
+        _user = user ?? throw new ArgumentNullException(nameof(user));
+        _resourceRegistry = resourceRegistry ?? throw new ArgumentNullException(nameof(resourceRegistry));
     }
 
-    return _resourceRegistry.GetResourceIds(orgNumber, cancellationToken);
-}
+    public async Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken)
+    {
+        var resourceIds = await GetCurrentUserResourceIds(cancellationToken);
+        return resourceIds.Contains(serviceResource);
+    }
+
+    public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken)
+    {
+        if (!_user.TryGetOrgNumber(out var orgNumber))
+        {
+            throw new UnreachableException();
+        }
+
+        return _resourceRegistry.GetResourceIds(orgNumber, cancellationToken);
+    }
 }
 
 internal sealed class LocalDevelopmentUserServiceDecorator : IUserService
 {
-private readonly IUserService _userService;
+    private readonly IUserService _userService;
 
-public LocalDevelopmentUserServiceDecorator(IUserService userService)
-{
-    _userService = userService ?? throw new ArgumentNullException(nameof(userService));
-}
+    public LocalDevelopmentUserServiceDecorator(IUserService userService)
+    {
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+    }
 
-public Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken) => 
-    Task.FromResult(true);
+    public Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken) => 
+        Task.FromResult(true);
 
-public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken) => 
-    _userService.GetCurrentUserResourceIds(cancellationToken);
+    public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken) => 
+        _userService.GetCurrentUserResourceIds(cancellationToken);
 }
