4a9edbba0867110fb362759a04f9584255bd382e
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2023-10-05 14:14:10 +0200

    Move 'UserService' into a named namespace.

--- src/Digdir.Domain.Dialogporten.Application/Common/IUserService.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/IUserService.cs
@@ -4,57 +4,56 @@ using Digdir.Domain.Dialogporten.Application.Externals;
 using System.Diagnostics;
 using Digdir.Domain.Dialogporten.Application.Common.Extensions;
 
-namespace Digdir.Domain.Dialogporten.Application.Common
+namespace Digdir.Domain.Dialogporten.Application.Common;
+
+internal interface IUserService
 {
-    internal interface IUserService
-    {
-        Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken);
-        Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken);
-    }
+    Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken);
+    Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken);
 }
 
 internal sealed class UserService : IUserService
 {
-    private readonly IUser _user;
-    private readonly IResourceRegistry _resourceRegistry;
+private readonly IUser _user;
+private readonly IResourceRegistry _resourceRegistry;
 
-    public UserService(
-        IUser user,
-        IResourceRegistry resourceRegistry)
+public UserService(
+    IUser user,
+    IResourceRegistry resourceRegistry)
+{
+    _user = user ?? throw new ArgumentNullException(nameof(user));
+    _resourceRegistry = resourceRegistry ?? throw new ArgumentNullException(nameof(resourceRegistry));
+}
+
+public async Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken)
+{
+    var resourceIds = await GetCurrentUserResourceIds(cancellationToken);
+    return resourceIds.Contains(serviceResource);
+}
+
+public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken)
+{
+    if (!_user.TryGetOrgNumber(out var orgNumber))
     {
-        _user = user ?? throw new ArgumentNullException(nameof(user));
-        _resourceRegistry = resourceRegistry ?? throw new ArgumentNullException(nameof(resourceRegistry));
+        throw new UnreachableException();
     }
 
-    public async Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken)
-    {
-        var resourceIds = await GetCurrentUserResourceIds(cancellationToken);
-        return resourceIds.Contains(serviceResource);
-    }
-
-    public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken)
-    {
-        if (!_user.TryGetOrgNumber(out var orgNumber))
-        {
-            throw new UnreachableException();
-        }
-
-        return _resourceRegistry.GetResourceIds(orgNumber, cancellationToken);
-    }
+    return _resourceRegistry.GetResourceIds(orgNumber, cancellationToken);
+}
 }
 
 internal sealed class LocalDevelopmentUserServiceDecorator : IUserService
 {
-    private readonly IUserService _userService;
+private readonly IUserService _userService;
 
-    public LocalDevelopmentUserServiceDecorator(IUserService userService)
-    {
-        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
-    }
+public LocalDevelopmentUserServiceDecorator(IUserService userService)
+{
+    _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+}
 
-    public Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken) => 
-        Task.FromResult(true);
+public Task<bool> CurrentUserIsOwner(string serviceResource, CancellationToken cancellationToken) => 
+    Task.FromResult(true);
 
-    public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken) => 
-        _userService.GetCurrentUserResourceIds(cancellationToken);
+public Task<IReadOnlyCollection<string>> GetCurrentUserResourceIds(CancellationToken cancellationToken) => 
+    _userService.GetCurrentUserResourceIds(cancellationToken);
 }
