d5650c085930b37329141f7cc24d154f9661caa4
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2023-10-10 07:30:20 +0200

    Add exception endpoint for testing (#207)

---
+++ src/Digdir.Domain.Dialogporten.WebApi/Endpoints/V1/Testing/ExceptionEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Digdir.Domain.Dialogporten.WebApi.Common.Authorization;
+using FastEndpoints;
+
+namespace Digdir.Domain.Dialogporten.WebApi.Endpoints.V1.Testing;
+
+public sealed class ExceptionEndpoint : Endpoint<ExceptionEndpointRequest>
+{
+    public override void Configure()
+    {
+        Get("Exception");
+        Policies(AuthorizationPolicy.Testing);
+        Description(x => x.ExcludeFromDescription());
+    }
+
+    public override Task HandleAsync(ExceptionEndpointRequest request, CancellationToken ct)
+    {
+        var exceptionType = Type.GetType(request.ExceptionType);
+
+        if (exceptionType is null || !exceptionType.IsAssignableTo(typeof(Exception)))
+        {
+            throw new Exception(request.ExceptionMessage);
+        }
+
+        throw (Exception)Activator.CreateInstance(exceptionType, request.ExceptionMessage)!;
+    }
+}
+
+public sealed class ExceptionEndpointRequest
+{
+    public string? ExceptionMessage { get; init; }
+    public string ExceptionType { get; init; } = "Exception";
+}
