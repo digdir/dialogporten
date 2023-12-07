9683f29393892c134872b71c6f968919ceb9e858
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2023-12-07 13:48:31 +0100

    Make forbidden great again.

--- src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/Forbidden.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/ReturnTypes/Forbidden.cs
@@ -2,9 +2,17 @@
 
 namespace Digdir.Domain.Dialogporten.Application.Common.ReturnTypes;
 
-public record Forbidden(IEnumerable<object> Scopes)
+public record Forbidden(List<string> Reasons)
 {
-    private string Message => $"Missing scopes: ({string.Join(", ", Scopes)}). ";
+    private const string ForbiddenMessage = "Forbidden";
 
-    public List<ValidationFailure> ToValidationResults() => new() { new ValidationFailure("Forbidden", Message) };
+    public Forbidden(params string[] reasons) : this(reasons.ToList()) { }
+
+    public List<ValidationFailure> ToValidationResults() => new(Reasons.Select(x => new ValidationFailure(ForbiddenMessage, x)));
+
+    public Forbidden WithMissingScopes(params string[] scopes)
+    {
+        Reasons.Add($"Missing scopes: ({string.Join(", ", scopes)}).");
+        return this;
+    }
 }
