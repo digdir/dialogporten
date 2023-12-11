18bd334513469f8e410260db71b75b34350f4d63
Ole Jørgen Skogstad <skogstad@softis.net>
2023-12-11 12:49:45 +0100

    More info logging

--- src/Digdir.Tool.Dialogporten.MigrationVerifier/MigrationVerifier.cs
+++ src/Digdir.Tool.Dialogporten.MigrationVerifier/MigrationVerifier.cs
@@ -31,6 +31,8 @@ public static class MigrationVerifier
         _httpClient.DefaultRequestHeaders.Remove("Authorization");
         _httpClient.DefaultRequestHeaders.Add("Authorization", $"Bearer {tokenResult.Token}");
 
+        logger.Information("### Executions URL: {ExecutionsUrl} ###", executionsUrl);
+        
         var retries = 0;
         while (retries++ < MaxRetries)
         {
@@ -47,6 +49,15 @@ public static class MigrationVerifier
                     continue;
                 }
 
+                logger.Information("### Found {ExecutionsCount} executions for job {JobName} ###",
+                    containerAppJobExecutions.Executions.Count, jobName);
+
+                foreach (var execution in containerAppJobExecutions.Executions)
+                {
+                    logger.Information("### Execution: ({Status}, {Image}) ###", 
+                        execution.Properties.Status, execution.Properties.Template.Containers[0].Image);
+                }
+                
                 var executionsForGitSha = containerAppJobExecutions.Executions
                     .Where(x => x.Properties.Template.Containers.Any(y => y.Image.Contains(gitSha)))
                     .ToList();
