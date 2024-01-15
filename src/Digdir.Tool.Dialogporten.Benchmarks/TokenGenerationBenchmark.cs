76bcd70e02682b76767c3d8a1b68d47e2adbf37a
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-01-15 09:16:45 +0100

    Cleanups

--- src/Digdir.Tool.Dialogporten.Benchmarks/TokenGenerationBenchmark.cs
+++ src/Digdir.Tool.Dialogporten.Benchmarks/TokenGenerationBenchmark.cs
@@ -1,6 +1,5 @@
 using System.Security.Cryptography;
 using System.Text;
-using System.Text.Json;
 using BenchmarkDotNet.Attributes;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
@@ -15,21 +14,23 @@ public class TokenGenerationBenchmark
     private RSA _rsa = null!;
     private ECDsa _ecdsa = null!;
     private AsymmetricCipherKeyPair _eddsaKeys = null!;
+    private Ed25519Signer _eddsaSigner = null!;
 
-    private readonly JsonDocument _claims = JsonDocument.Parse(@"
-        {
-            ""l"": 4,
-            ""c"": ""urn:altinn:person:identifier-no::12018212345"",
-            ""p"": ""urn:altinn:organization:identifier-no::991825827"",
-            ""s"": ""urn:altinn:organization:identifier-no::825827991"",
-            ""i"": ""e0300961-85fb-4ef2-abff-681d77f9960e"",
-            ""u"": ""https://example.com/api/dialogs/123456789/dialogelements/5b5446a7.pdf"",
-            ""exp"": 1672772834,
-            ""iss"": ""https://dialogporten.no"",
-            ""nbf"": 1672771934,
-            ""iat"": 1672771934 
-        }");
-
+    private readonly byte[] _payload = Encoding.UTF8.GetBytes(Base64UrlEncode(
+        """
+          {
+              "l": 4,
+              "c": "urn:altinn:person:identifier-no::12018212345",
+              "p": "urn:altinn:organization:identifier-no::991825827",
+              "s": "urn:altinn:organization:identifier-no::825827991",
+              "i": "e0300961-85fb-4ef2-abff-681d77f9960e",
+              "u": "https://example.com/api/dialogs/123456789/dialogelements/5b5446a7.pdf",
+              "exp": 1672772834,
+              "iss": "https://dialogporten.no",
+              "nbf": 1672771934,
+              "iat": 1672771934
+          }
+          """));
     [GlobalSetup]
     public void Setup()
     {
@@ -44,90 +45,33 @@ public class TokenGenerationBenchmark
         var generator = new Ed25519KeyPairGenerator();
         generator.Init(new KeyGenerationParameters(new SecureRandom(), 256));
         _eddsaKeys = generator.GenerateKeyPair();
+        _eddsaSigner = new Ed25519Signer();
+        _eddsaSigner.Init(true, _eddsaKeys.Private);
     }
 
     [Benchmark]
-    public string GenerateTokenWithRsa()
+    public void GenerateTokenWithRsa()
     {
-        var header = JsonSerializer.Serialize(new
-        {
-            alg = "RS256",
-            typ = "JWT"
-        });
-
-        var payload = JsonSerializer.Serialize(_claims);
-
-        return GenerateJwtToken(header, payload, _rsa, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
+        _rsa.SignData(_payload, HashAlgorithmName.SHA256, RSASignaturePadding.Pkcs1);
     }
 
     [Benchmark]
-    public string GenerateTokenWithEcdsa()
+    public void GenerateTokenWithEcdsa()
     {
-        var header = JsonSerializer.Serialize(new
-        {
-            alg = "ES256",
-            typ = "JWT"
-        });
-
-        var payload = JsonSerializer.Serialize(_claims);
-
-        return GenerateJwtToken(header, payload, _ecdsa, HashAlgorithmName.SHA256);
+        _ecdsa.SignData(_payload, HashAlgorithmName.SHA256);
     }
 
     [Benchmark]
-    public string GenerateTokenWithEdDsa()
+    public void GenerateTokenWithEdDsa()
     {
-        // Prepare EdDSA token
-        var signer = new Ed25519Signer();
-        signer.Init(true, _eddsaKeys.Private);
-
-        // Serialize claims to JSON
-        var claimsJson = JsonSerializer.Serialize(_claims);
-
-        // Sign the data
-        var data = Encoding.UTF8.GetBytes(claimsJson);
-        signer.BlockUpdate(data, 0, data.Length);
-        var signature = signer.GenerateSignature();
-
-        // Construct the token (you need to define the exact format)
-        return Base64UrlEncode(signature);
-    }
-
-    private static string GenerateJwtToken(string header, string payload, RSA rsa, HashAlgorithmName hashAlgorithm, RSASignaturePadding padding)
-    {
-        var headerBase64 = Base64UrlEncode(header);
-        var payloadBase64 = Base64UrlEncode(payload);
-
-        var data = Encoding.UTF8.GetBytes($"{headerBase64}.{payloadBase64}");
-
-        var signature = rsa.SignData(data, hashAlgorithm, padding);
-        var signatureBase64 = Base64UrlEncode(signature);
-
-        return $"{headerBase64}.{payloadBase64}.{signatureBase64}";
-    }
-
-    private static string GenerateJwtToken(string header, string payload, ECDsa ecdsa, HashAlgorithmName hashAlgorithm)
-    {
-        var headerBase64 = Base64UrlEncode(header);
-        var payloadBase64 = Base64UrlEncode(payload);
-
-        var data = Encoding.UTF8.GetBytes($"{headerBase64}.{payloadBase64}");
-
-        var signature = ecdsa.SignData(data, hashAlgorithm);
-        var signatureBase64 = Base64UrlEncode(signature);
-
-        return $"{headerBase64}.{payloadBase64}.{signatureBase64}";
+        _eddsaSigner.Init(true, _eddsaKeys.Private);
+        _eddsaSigner.BlockUpdate(_payload, 0, _payload.Length);
+        _eddsaSigner.GenerateSignature();
     }
 
     private static string Base64UrlEncode(string input)
     {
-        var inputBytes = Encoding.UTF8.GetBytes(input);
-        return Base64UrlEncode(inputBytes);
-    }
-
-    private static string Base64UrlEncode(byte[] input)
-    {
-        return Convert.ToBase64String(input)
+        return Convert.ToBase64String(Encoding.UTF8.GetBytes(input))
                       .TrimEnd('=')
                       .Replace('+', '-')
                       .Replace('/', '_');
