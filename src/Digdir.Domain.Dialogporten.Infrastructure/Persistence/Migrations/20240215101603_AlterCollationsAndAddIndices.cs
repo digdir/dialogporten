d739d6504b0e1dfe5f31e38317c42aeb844699ea
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-02-15 11:35:52 +0100

    Add C collation and indices to Party and ServiceResource columns, rework local AA implementation

---
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Migrations/20240215101603_AlterCollationsAndAddIndices.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digdir.Domain.Dialogporten.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlterCollationsAndAddIndices : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Change collation to "C" for faster indices.
+            // Add b-tree indices for ServiceResource and Party as they are heavily used in queries.
+            migrationBuilder.Sql(
+                """
+                 ALTER TABLE "Dialog" ALTER COLUMN "Org" TYPE character varying(255) COLLATE "C" USING "Org"::varchar;
+                 ALTER TABLE "Dialog" ALTER COLUMN "ServiceResource" TYPE character varying(255) COLLATE "C" USING "ServiceResource"::varchar;
+                 ALTER TABLE "Dialog" ALTER COLUMN "Party" TYPE character varying(255) COLLATE "C" USING "Party"::varchar;
+             
+                 CREATE INDEX IF NOT EXISTS "IX_Dialog_ServiceResource" ON "Dialog" USING btree ("ServiceResource");
+                 CREATE INDEX IF NOT EXISTS "IX_Dialog_Party" ON "Dialog" USING btree ("Party");
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Revert collation to default.
+            migrationBuilder.Sql(
+                """
+                 DROP INDEX IF EXISTS "IX_Dialog_ServiceResource";
+                 DROP INDEX IF EXISTS "IX_Dialog_Party";
+
+                 ALTER TABLE "Dialog" ALTER COLUMN "Org" TYPE character varying(255) USING "Org"::varchar;
+                 ALTER TABLE "Dialog" ALTER COLUMN "ServiceResource" TYPE character varying(255) USING "ServiceResource"::varchar;
+                 ALTER TABLE "Dialog" ALTER COLUMN "Party" TYPE character varying(255) USING "Party"::varchar;
+               """);
+        }
+    }
+}
