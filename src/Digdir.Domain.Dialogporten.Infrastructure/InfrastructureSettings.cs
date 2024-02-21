2a2c474ba0c292f8608f1aa4ad082102e792c504
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-02-21 12:50:49 +0100

    Revert local auth settings, reintroduce subscription key

--- src/Digdir.Domain.Dialogporten.Infrastructure/InfrastructureSettings.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/InfrastructureSettings.cs
@@ -17,6 +17,7 @@ public sealed class InfrastructureSettings
 public sealed class AltinnPlatformSettings
 {
     public required Uri BaseUri { get; init; }
+    public required string SubscriptionKey { get; init; }
 }
 
 public sealed class AltinnCdnPlatformSettings
