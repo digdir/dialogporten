c5c426ee4ba9dffca8aba6fc54fe214c760d95ad
Ole Jørgen Skogstad <skogstad@softis.net>
2024-03-13 14:04:25 +0100

    chore: Improve error message for purge command (#551)

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Purge/PurgeDialogCommandValidator.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Purge/PurgeDialogCommandValidator.cs
@@ -7,6 +7,7 @@ internal sealed class PurgeDialogCommandValidator : AbstractValidator<PurgeDialo
     public PurgeDialogCommandValidator()
     {
         RuleFor(x => x.DialogId)
-            .NotEqual(default(Guid));
+            .NotEqual(Guid.Empty)
+            .WithMessage($"{{PropertyName}} was either badly formatted or {Guid.Empty}");
     }
 }
