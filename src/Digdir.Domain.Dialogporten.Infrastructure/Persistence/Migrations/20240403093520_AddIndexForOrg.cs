a4c19530f238e61c046fa9cb8f3b366a7fefebad
Ole Jørgen Skogstad <skogstad@softis.net>
2024-04-03 14:32:17 +0200

    feat: Add db index for Dialog.Org (#584)

---
+++ src/Digdir.Domain.Dialogporten.Infrastructure/Persistence/Migrations/20240403093520_AddIndexForOrg.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Digdir.Domain.Dialogporten.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIndexForOrg : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                 CREATE INDEX IF NOT EXISTS "IX_Dialog_Org" ON "Dialog" USING btree ("Org");
+                """);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                  DROP INDEX IF EXISTS "IX_Dialog_Org";
+                """);
+        }
+    }
+}
