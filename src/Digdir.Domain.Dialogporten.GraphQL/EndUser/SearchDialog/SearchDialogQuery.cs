ad0f37934651802c2137d9dc5a7d5027ad57615a
Ole Jørgen Skogstad <skogstad@softis.net>
2024-04-15 17:05:16 +0200

    --wip-- [skip ci]

--- src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialog/SearchDialogQuery.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialog/SearchDialogQuery.cs
@@ -1,5 +1,4 @@
 using AutoMapper;
-using Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.Dialogs.Queries.Get;
 using MediatR;
 
 namespace Digdir.Domain.Dialogporten.GraphQL.EndUser.SearchDialog;
