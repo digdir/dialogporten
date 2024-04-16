45cc73c5fe40f2a4d0ac0cdfe430d501de2366b1
Ole Jørgen Skogstad <skogstad@softis.net>
2024-04-16 10:21:29 +0200

    --wip-- [skip ci]

--- src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialogs/MappingProfile.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialogs/MappingProfile.cs
@@ -17,26 +17,5 @@ public class MappingProfile : Profile
         CreateMap<PaginatedList<SearchDialogDto>, SearchDialogsPayload>();
 
         CreateMap<SearchDialogDto, SearchDialog>();
-        // CreateMap<GetDialogDto, DialogSearch>()
-        //     .ForMember(dest => dest.StatusSearch, opt => opt.MapFrom(src => src.Status));
-        //
-        // CreateMap<GetDialogContentDto, ContentSearch>()
-        //     .ForMember(dest => dest.TypeSearch, opt => opt.MapFrom(src => src.Type));
-        //
-        // CreateMap<GetDialogDialogElementDto, ElementSearch>();
-        // CreateMap<GetDialogDialogElementUrlDto, ElementUrlSearch>()
-        //     .ForMember(dest => dest.ConsumerSearchType, opt => opt.MapFrom(src => src.ConsumerType));
-        //
-        // CreateMap<GetDialogDialogGuiActionDto, GuiActionSearch>()
-        //     .ForMember(dest => dest.PrioritySearch, opt => opt.MapFrom(src => src.Priority));
-        //
-        // CreateMap<GetDialogDialogApiActionDto, ApiActionSearch>();
-        // CreateMap<GetDialogDialogApiActionEndpointDto, ApiActionEndpointSearch>()
-        //     .ForMember(dest => dest.HttpMethod, opt => opt.MapFrom(src => src.HttpMethod));
-        //
-        // CreateMap<GetDialogDialogActivityDto, ActivitySearch>()
-        //     .ForMember(dest => dest.TypeSearch, opt => opt.MapFrom(src => src.Type));
-        //
-        // CreateMap<GetDialogDialogSeenLogDto, SeenLogSearch>();
     }
 }
