c276c09c864806344b1cd13442fd47fbaec63393
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-04-20 17:47:22 +0200

    Fix K6 summary handling

--- tests/k6/common/summary.js
+++ tests/k6/common/summary.js
@@ -3,7 +3,8 @@ import { generateJUnitXML, textSummary } from "./report.js";
 export default function (data) {
     return {
         'stdout': textSummary(data, { indent: ' ', enableColors: true }) + "\n",
-        'summary.json': JSON.stringify(data),
-        'junit.xml': generateJUnitXML(data, 'dialogporten')
+        //'summary.txt': textSummary(data, { indent: ' ', enableColors: false }) + "\n",
+        //'summary.json': JSON.stringify(data),
+        'junit.xml': generateJUnitXML(data)
     };
 }
