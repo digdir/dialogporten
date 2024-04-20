553b29d86e33bc37773c91bf6151cf8e090c6ec3
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-04-20 18:07:45 +0200

    Fix escaping

--- tests/k6/common/report.js
+++ tests/k6/common/report.js
@@ -5,15 +5,27 @@ export function generateJUnitXML(k6Json) {
     xmlDoc.push('<?xml version="1.0" encoding="UTF-8" ?>');
     xmlDoc.push('<testsuites>');
 
+    function xmlEncode(string) {
+        return string.replace(/&/g, '&amp;')
+                     .replace(/</g, '&lt;')
+                     .replace(/>/g, '&gt;')
+                     .replace(/"/g, '&quot;')
+                     .replace(/'/g, '&apos;');
+    }
+
     function processGroup(group) {
         if (group.name) { // skip root group
-            xmlDoc.push(`<testsuite name="${group.name}" tests="${group.checks.length}">`);
+
+            const checkName = xmlEncode(check.name);
+            const groupName = xmlEncode(group.name);
+
+            xmlDoc.push(`<testsuite name="${groupName}" tests="${group.checks.length}">`);
 
             group.checks.forEach(check => {
                 let failed = check.fails > 0 ;
-                xmlDoc.push(`<testcase classname="${group.name}" name="${check.name}">`);
+                xmlDoc.push(`<testcase classname="${groupName}" name="${checkName}">`);
                 if (failed) {
-                    xmlDoc.push(`<failure message="Check failed. See output K6 task for more details.">${check.name}</failure>`);
+                    xmlDoc.push(`<failure message="Check failed. See output K6 task for more details.">${checkName}</failure>`);
                 }
                 xmlDoc.push('</testcase>');
             });
