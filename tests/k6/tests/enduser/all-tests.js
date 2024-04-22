7757ce99a3d9c35381b30615a2b80f67371e5b28
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-04-22 22:39:57 +0200

    Add simple e2e test

--- tests/k6/tests/enduser/all-tests.js
+++ tests/k6/tests/enduser/all-tests.js
@@ -1,8 +1,10 @@
 // This file is generated, see "scripts" directory
 import { default as dialogDetails } from './dialogDetails.js';
 import { default as dialogSearch } from './dialogSearch.js';
+import { default as parties } from './parties.js';
 
 export default function() {
   dialogDetails();
   dialogSearch();
+  parties();
 }
