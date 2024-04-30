2127c1c1fdaf35db4de7cba51ff2289d8fa69101
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-04-30 14:01:45 +0200

    Add upstream error exception wrapper

--- src/Digdir.Domain.Dialogporten.Infrastructure/HttpClientExtensions.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/HttpClientExtensions.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
 using System.Text.Json;
+using Digdir.Domain.Dialogporten.Infrastructure.Common.Exceptions;
 
 namespace Digdir.Domain.Dialogporten.Infrastructure;
 
@@ -12,14 +13,21 @@ public static class HttpClientExtensions
         Action<HttpRequestHeaders>? configureHeaders = null,
         CancellationToken cancellationToken = default)
     {
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
-        configureHeaders?.Invoke(httpRequestMessage.Headers);
-        var response = await client.SendAsync(httpRequestMessage, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        var result = await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
-        return result is null
-            ? throw new JsonException($"Failed to deserialize JSON to type {typeof(T).FullName} from {requestUri}")
-            : result;
+        try
+        {
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, requestUri);
+            configureHeaders?.Invoke(httpRequestMessage.Headers);
+            var response = await client.SendAsync(httpRequestMessage, cancellationToken);
+            response.EnsureSuccessStatusCode();
+            var result = await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+            return result is null
+                ? throw new JsonException($"Failed to deserialize JSON to type {typeof(T).FullName} from {requestUri}")
+                : result;
+        }
+        catch (Exception e)
+        {
+            throw new UpstreamServiceException(e);
+        }
     }
 
     public static async Task<HttpResponseMessage> PostAsJsonEnsuredAsync(
@@ -31,15 +39,22 @@ public static class HttpClientExtensions
         JsonSerializerOptions? serializerOptions = null,
         CancellationToken cancellationToken = default)
     {
-        var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri)
+        try
         {
-            Content = JsonContent.Create(content, options: serializerOptions)
-        };
-        configureHeaders?.Invoke(httpRequestMessage.Headers);
-        configureContentHeaders?.Invoke(httpRequestMessage.Content.Headers);
-        var response = await client.SendAsync(httpRequestMessage, cancellationToken);
-        response.EnsureSuccessStatusCode();
-        return response;
+            var httpRequestMessage = new HttpRequestMessage(HttpMethod.Post, requestUri)
+            {
+                Content = JsonContent.Create(content, options: serializerOptions)
+            };
+            configureHeaders?.Invoke(httpRequestMessage.Headers);
+            configureContentHeaders?.Invoke(httpRequestMessage.Content.Headers);
+            var response = await client.SendAsync(httpRequestMessage, cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return response;
+        }
+        catch (Exception e)
+        {
+            throw new UpstreamServiceException(e);
+        }
     }
 
     public static async Task<T> PostAsJsonEnsuredAsync<T>(
@@ -53,9 +68,16 @@ public static class HttpClientExtensions
     {
         var response = await client.PostAsJsonEnsuredAsync(requestUri, content, configureHeaders,
             configureContentHeaders, serializerOptions, cancellationToken);
-        var result = await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
-        return result is null
-            ? throw new JsonException($"Failed to deserialize JSON to type {typeof(T).FullName} from {requestUri}")
-            : result;
+        try
+        {
+            var result = await response.Content.ReadFromJsonAsync<T>(cancellationToken: cancellationToken);
+            return result is null
+                ? throw new JsonException($"Failed to deserialize JSON to type {typeof(T).FullName} from {requestUri}")
+                : result;
+        }
+        catch (Exception e)
+        {
+            throw new UpstreamServiceException(e);
+        }
     }
 }
