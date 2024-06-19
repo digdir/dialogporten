46729d95b426aa584a7c7e8797d545614d041309
Ole Jørgen Skogstad <skogstad@softis.net>
2024-06-19 11:37:19 +0200

    clean

--- src/Digdir.Domain.Dialogporten.Domain/MediaTypes.cs
+++ src/Digdir.Domain.Dialogporten.Domain/MediaTypes.cs
@@ -3,9 +3,10 @@ namespace Digdir.Domain.Dialogporten.Domain;
 
 public static class MediaTypes
 {
+    public static string EmbeddablePrefix => "application/vnd.dialogporten.frontchannelembed";
     public static string EmbeddableMarkdown => $"{EmbeddablePrefix}+json;type=markdown";
+
     public static string Markdown => "text/markdown";
     public static string PlainText => "text/plain";
     public static string Html => "text/html";
-    public static string EmbeddablePrefix => "application/vnd.dialogporten.frontchannelembed";
 }
