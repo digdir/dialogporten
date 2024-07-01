d05fb4d7a67d35941aae09af4e71e54520bd5d47
Ole Jørgen Skogstad <skogstad@softis.net>
2024-07-01 15:10:34 +0200

    check

--- tests/k6/tests/serviceowner/concurrency.js
+++ tests/k6/tests/serviceowner/concurrency.js
@@ -4,22 +4,22 @@ import { default as dialogToInsert } from './testdata/01-create-dialog.js';
 export default function () {
 
     let dialogId = null;
- 
+
     describe('Perform dialog create', () => {
         let r = postSO('dialogs', dialogToInsert());
         expectStatusFor(r).to.equal(201);
         expect(r, 'response').to.have.validJsonBody();
         expect(r.json(), 'response json').to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/)
-        
+
         dialogId = r.json();
     });
 
     describe('Attempt to add a few child entities concurrently', async () => {
 
         let promises = [];
-        
+
         for (let i=0; i<10; i++) {
-            let activity = { type: "Information", description: [ { value: i.toString(), cultureCode: "nb-no"}]};
+            let activity = { type: "Information", description: [ { value: i.toString(), languageCode: "nb"}]};
             promises.push(postSOAsync('dialogs/' + dialogId + '/activities?' + i, activity))
         }
 
@@ -31,6 +31,6 @@ export default function () {
         results.forEach((r) => {
             expect(r.status, 'status code for concurrently added child entity').to.equal(201);
         });
-   
+
     });
 }
