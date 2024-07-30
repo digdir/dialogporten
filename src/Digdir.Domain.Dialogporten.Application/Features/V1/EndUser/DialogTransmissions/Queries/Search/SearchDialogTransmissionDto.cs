d608adebadc12d099c04c4b174569392837603e6
Ole Jørgen Skogstad <skogstad@softis.net>
2024-07-30 15:18:00 +0200

    feat(WebAPI): Add Transmission endpoints  (#943)

---
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/EndUser/DialogTransmissions/Queries/Search/SearchDialogTransmissionDto.cs
@@ -0,0 +1,56 @@
+using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Content;
+using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Localizations;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Actors;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Attachments;
+using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Transmissions;
+
+namespace Digdir.Domain.Dialogporten.Application.Features.V1.EndUser.DialogTransmissions.Queries.Search;
+
+public sealed class SearchDialogTransmissionDto
+{
+    public Guid Id { get; set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public string? AuthorizationAttribute { get; set; }
+    public bool IsAuthorized { get; set; }
+    public string? ExtendedType { get; set; }
+    public Guid? RelatedTransmissionId { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
+
+    public DialogTransmissionType.Values Type { get; set; }
+
+    public SearchDialogTransmissionSenderActorDto Sender { get; set; } = null!;
+
+    public SearchDialogTransmissionContentDto Content { get; set; } = null!;
+    public List<SearchDialogTransmissionAttachmentDto> Attachments { get; set; } = [];
+}
+
+public sealed class SearchDialogTransmissionSenderActorDto
+{
+    public Guid Id { get; set; }
+    public DialogActorType.Values ActorType { get; set; }
+    public string ActorName { get; set; } = null!;
+    public string ActorId { get; set; } = null!;
+}
+
+public sealed class SearchDialogTransmissionContentDto
+{
+    public ContentValueDto Title { get; set; } = null!;
+    public ContentValueDto Summary { get; set; } = null!;
+}
+
+public sealed class SearchDialogTransmissionAttachmentDto
+{
+    public Guid Id { get; set; }
+
+    public List<LocalizationDto> DisplayName { get; set; } = [];
+    public List<SearchDialogTransmissionAttachmentUrlDto> Urls { get; set; } = [];
+}
+
+public sealed class SearchDialogTransmissionAttachmentUrlDto
+{
+    public Guid Id { get; set; }
+    public Uri Url { get; set; } = null!;
+    public string? MediaType { get; set; } = null!;
+
+    public AttachmentUrlConsumerType.Values ConsumerType { get; set; }
+}
