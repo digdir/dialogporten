75a1d9ab9d33ac30c850e76661b8a64b62e60e9b
Ole Jørgen Skogstad <skogstad@softis.net>
2024-08-30 10:26:22 +0200

    fix

--- src/Digdir.Domain.Dialogporten.Domain/Common/UuidV7.cs
+++ src/Digdir.Domain.Dialogporten.Domain/Common/UuidV7.cs
@@ -10,32 +10,20 @@ public static class UuidV7
     public static bool TryParse(ReadOnlySpan<char> value, out Guid result)
         => Guid.TryParse(value, out result) && IsValid(result);
 
-    public static bool IsValid(Guid value) => IsValid(ToBigEndianByteArray(value));
+    public static bool IsValid(Guid value) => IsValid(value.ToByteArray());
 
     public static bool TryToDateTimeOffset(Guid value, out DateTimeOffset result)
-    {
-        var bytes = ToBigEndianByteArray(value);
-        return TryToDateTimeOffset(bytes, out result);
-    }
-
-    /// <summary>
-    /// Converts a Guid to a big endian byte array. We expect big endian Guids throughout the system.
-    /// However, the default behavior of Guids is to treat them as little endian.
-    /// This method is to make it painfully obvious to the reader.
-    /// </summary>
-    /// <param name="value">The Guid to convert</param>
-    /// <returns></returns>
-    private static ReadOnlySpan<byte> ToBigEndianByteArray(Guid value) => value.ToByteArray(bigEndian: true);
+        => TryToDateTimeOffset(value.ToByteArray(), out result);
 
     private static bool TryToDateTimeOffset(ReadOnlySpan<byte> bytes, out DateTimeOffset result)
     {
-        // The timestamp is stored in the first 6 bytes, in big endian order.
-        var unixMs = ((long)bytes[0] << 40)
-                     | ((long)bytes[1] << 32)
-                     | ((long)bytes[2] << 24)
-                     | ((long)bytes[3] << 16)
-                     | ((long)bytes[4] << 8)
-                     | bytes[5];
+        // The timestamp is stored in the first 6 bytes, in little endian order.
+        var unixMs = ((long)bytes[3] << 40)
+                     | ((long)bytes[2] << 32)
+                     | ((long)bytes[1] << 24)
+                     | ((long)bytes[0] << 16)
+                     | ((long)bytes[5] << 8)
+                     | bytes[4];
 
         var ticks = (UnixEpochMilliseconds + unixMs) * TicksPerMillisecond;
 
@@ -52,7 +40,7 @@ public static class UuidV7
     private static bool IsValid(ReadOnlySpan<byte> bytes)
     {
         // The version is stored in the 7th byte, but the nibbles are reversed so version is actually in the higher 4 bits.
-        var version = bytes[6] >> 4;
+        var version = bytes[7] >> 4;
 
         // The variant is stored in the 9th byte, in the higher bits.
         var variant = bytes[8] >> 6;
