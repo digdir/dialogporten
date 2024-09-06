c0a6e5a0c39ddc64c6c4ecc11b892755e0180802
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2024-09-06 10:47:32 +0200

    Add doc

--- src/Digdir.Domain.Dialogporten.Application/Common/Extensions/ConfigurationExtensions.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/Extensions/ConfigurationExtensions.cs
@@ -7,6 +7,13 @@ public static class ConfigurationExtensions
 {
     private const string AppsettingsLocalJson = "appsettings.local.json";
 
+
+    /// <summary>
+    /// Adds the local configuration file (appsettings.local.json) to the configuration builder if the environment is Development.
+    /// </summary>
+    /// <param name="builder">The configuration builder to add the local configuration file to.</param>
+    /// <param name="environment">The host environment to check if it is in development.</param>
+    /// <returns>The configuration builder with the local configuration file added if in development.</returns>
     public static IConfigurationBuilder AddLocalConfiguration(this IConfigurationBuilder builder, IHostEnvironment environment)
         => !environment.IsDevelopment() ? builder : builder.AddJsonFile(AppsettingsLocalJson, optional: true, reloadOnChange: true);
 }
