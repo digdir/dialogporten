5ba84b9c79f8b9a8c295e500a9c4222a2c08bbe1
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2024-09-16 22:04:59 +0200

    Small changes

--- tests/Digdir.Domain.Dialogporten.Architecture.Tests/SealedClassesTest.cs
+++ tests/Digdir.Domain.Dialogporten.Architecture.Tests/SealedClassesTest.cs
@@ -7,30 +7,26 @@ public class SealedClassesTest
     [Fact]
     public void All_Classes_Without_Inheritors_Should_Be_Sealed()
     {
-        var allTypes = Types
+        var nonAbstractClasses = Types
             .InAssemblies(DialogportenAssemblies.All)
-            .That()
-            .AreClasses()
-            .And()
-            .AreNotAbstract()
+            .That().AreClasses()
+            .And().AreNotAbstract()
+            .And().DoNotHaveName(nameof(Program))
             .GetTypes()
-            .Where(x => x.Name != nameof(Program))
             .ToList();
 
-        var typesWithInheritors = new HashSet<Type>(
-            allTypes.Where(t => t.BaseType != null)
-                .Select(t => t.BaseType!.IsGenericType ?
-                    t.BaseType.GetGenericTypeDefinition() : t.BaseType)
-        );
+        var inheritedTypes = nonAbstractClasses
+            .Where(x => x.BaseType is not null)
+            .Select(t => t.BaseType!.IsGenericType
+                ? t.BaseType.GetGenericTypeDefinition()
+                : t.BaseType)
+            .ToHashSet();
 
-        var nonInheritedTypes = allTypes
-            .Where(t => !typesWithInheritors.Contains(t))
+        var notSealedNorInheritedTypes = nonAbstractClasses
+            .Except(inheritedTypes)
+            .Where(x => !x.IsSealed)
             .ToList();
 
-        var failingTypes = nonInheritedTypes
-            .Where(t => !t.IsSealed)
-            .ToList();
-
-        Assert.Empty(failingTypes);
+        Assert.Empty(notSealedNorInheritedTypes);
     }
 }
