3e8ff8fae1cdf1c01e2fe23d36f48515f63c7f07
Amund Myrbostad <amund.myrbostad@digdir.no>
2024-09-16 16:11:58 +0200

    Added cleanup in POST test

--- tests/k6/tests/serviceowner/dialogCreateCorrespondenceResource.js
+++ tests/k6/tests/serviceowner/dialogCreateCorrespondenceResource.js
@@ -1,8 +1,11 @@
-import {describe, expect, expectStatusFor, postSO} from '../../common/testimports.js'
+import {describe, expect, expectStatusFor, postSO, purgeSO} from '../../common/testimports.js'
 import {default as dialogToInsert} from './testdata/01-create-dialog.js';
 
 export default function () {
-
+    let dialogIds = [];
+    let tokenOptions = {
+        scopes: "digdir:dialogporten digdir:dialogporten.serviceprovider digdir:dialogporten.serviceprovider.search digdir:dialogporten.correspondence"
+    }
     describe('dialog create correspondence resource without correct scope ', () => {
         let dialog = dialogToInsert();
         dialog.serviceResource = "urn:altinn:resource:ttd-dialogporten-automated-tests-correspondence";
@@ -15,12 +18,17 @@ export default function () {
     describe('dialog create correspondence resource with correct scope', () => {
         let dialog = dialogToInsert();
         dialog.serviceResource = "urn:altinn:resource:ttd-dialogporten-automated-tests-correspondence";
-        let response = postSO('dialogs', dialog, null, {
-            scopes: "digdir:dialogporten digdir:dialogporten.serviceprovider digdir:dialogporten.serviceprovider.search digdir:dialogporten.correspondence"
-        })
+        let response = postSO('dialogs', dialog, null, tokenOptions)
         expectStatusFor(response).to.equal(201);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response json').to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/)
-
+        dialogIds.push(response.json());
     })
+
+    describe("Cleanup", () => {
+        dialogIds.forEach((d) => {
+            let r = purgeSO("dialogs/" + d, null, tokenOptions);
+            expect(r.status, 'response status').to.equal(204);
+        });
+    });
 }
