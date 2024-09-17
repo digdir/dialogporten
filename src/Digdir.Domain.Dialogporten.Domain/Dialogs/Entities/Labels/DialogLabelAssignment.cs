3aa5095ea8b12c250155dba7c4fbae7bc1593add
Amund Myrbostad <amund.myrbostad@digdir.no>
2024-09-17 15:41:14 +0200

    DialogLabel, DialogLabelAssignment updated

--- src/Digdir.Domain.Dialogporten.Domain/Dialogs/Entities/Labels/DialogLabelAssignment.cs
+++ src/Digdir.Domain.Dialogporten.Domain/Dialogs/Entities/Labels/DialogLabelAssignment.cs
@@ -1,11 +1,17 @@
 using Digdir.Library.Entity.Abstractions;
+using Digdir.Library.Entity.Abstractions.Features.Identifiable;
+using Digdir.Library.Entity.Abstractions.Features.SoftDeletable;
 
 namespace Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Labels;
 
 // Amund: Er IEntity rett her? he ikkje fonne en koblings entitet å sammenligne mæ endå. 
-public class DialogLabelAssignment : IEntity
+public class DialogLabelAssignment : IIdentifiableEntity, ISoftDeletableEntity
 {
     public Guid Id { get; set; }
-    public DateTimeOffset CreatedAt { get; set; }
-    public DateTimeOffset UpdatedAt { get; set; }
+
+    public Guid LabelId { get; set; }
+
+    public Guid DialogId { get; set; }
+    public bool Deleted { get; set; }
+    public DateTimeOffset? DeletedAt { get; set; }
 }
