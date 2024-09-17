9a1d889b8c5089ee3061a42a3fb433362ba48d2c
Amund Myrbostad <amund.myrbostad@digdir.no>
2024-09-17 10:31:05 +0200

    update from let to const

--- tests/k6/tests/serviceowner/dialogCreateUpdatePatchDeleteCorrespondenceResource.js
+++ tests/k6/tests/serviceowner/dialogCreateUpdatePatchDeleteCorrespondenceResource.js
@@ -2,24 +2,24 @@ import {describe, expect, expectStatusFor, postSO, purgeSO, putSO, patchSO} from
 import {default as dialogToInsert} from './testdata/01-create-dialog.js';
 
 export default function () {
-    let dialogIds = [];
-    let dialogs = [];
-    let tokenOptions = {
+    const dialogIds = [];
+    const dialogs = [];
+    const tokenOptions = {
         scopes: "digdir:dialogporten digdir:dialogporten.serviceprovider digdir:dialogporten.serviceprovider.search digdir:dialogporten.correspondence"
     }
     describe('create dialog with correspondence  resource without correct scope', () => {
-        let dialog = dialogToInsert();
+        const dialog = dialogToInsert();
         dialog.serviceResource = "urn:altinn:resource:ttd-dialogporten-automated-tests-correspondence";
-        let response = postSO('dialogs', dialog);
+        const response = postSO('dialogs', dialog);
         expectStatusFor(response).to.equal(403);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response body').to.have.property('errors');
     })
 
     describe('create dialog with correspondence resource with correct scope', () => {
-        let dialog = dialogToInsert();
+        const dialog = dialogToInsert();
         dialog.serviceResource = "urn:altinn:resource:ttd-dialogporten-automated-tests-correspondence";
-        let response = postSO('dialogs', dialog, null, tokenOptions)
+        const response = postSO('dialogs', dialog, null, tokenOptions)
         expectStatusFor(response).to.equal(201);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response json').to.match(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/)
@@ -28,11 +28,11 @@ export default function () {
     })
 
     describe('update dialog with correspondence resource without correct scope', () => {
-        let dialog = dialogs[dialogs.length - 1];
-        let dialogId = dialogIds[dialogs.length - 1];
+        const dialog = dialogs[dialogs.length - 1];
+        const dialogId = dialogIds[dialogs.length - 1];
         dialog.progress = 80;
 
-        let response = putSO('dialogs/' + dialogId, dialog);
+        const response = putSO('dialogs/' + dialogId, dialog);
         expectStatusFor(response).to.equal(403);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response body').to.have.property('errors');
@@ -40,55 +40,55 @@ export default function () {
     })
 
     describe('update dialog with correspondence resource with correct scope', () => {
-        let dialog = dialogs[dialogs.length - 1];
-        let dialogId = dialogIds[dialogs.length - 1];
+        const dialog = dialogs[dialogs.length - 1];
+        const dialogId = dialogIds[dialogs.length - 1];
         dialog.progress = 80;
 
-        let response = putSO('dialogs/' + dialogId, dialog, null, tokenOptions);
+        const response = putSO('dialogs/' + dialogId, dialog, null, tokenOptions);
         expectStatusFor(response).to.equal(204);
         dialogs[dialogs.length - 1] = dialog;
 
     })
     describe('patch dialog with correspondence resource without correct scope', () => {
-        let dialogId = dialogIds[dialogs.length - 1];
-        let patch = [
+        const dialogId = dialogIds[dialogs.length - 1];
+        const patch = [
             {
                 'op': 'replace',
                 'path': '/progress',
                 'value': 80
             }
         ]
-        let response = patchSO('dialogs/' + dialogId, patch);
+        const response = patchSO('dialogs/' + dialogId, patch);
         expectStatusFor(response).to.equal(403);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response body').to.have.property('errors');
     })
     
     describe('patch dialog with correspondence resource with correct scope', () => {
-        let dialogId = dialogIds[dialogs.length - 1];
-        let patch = [
+        const dialogId = dialogIds[dialogs.length - 1];
+        const patch = [
             {
                 'op': 'replace',
                 'path': '/progress',
                 'value': 80
             }
         ]
-        let response = patchSO('dialogs/' + dialogId, patch, null, tokenOptions);
+        const response = patchSO('dialogs/' + dialogId, patch, null, tokenOptions);
         expectStatusFor(response).to.equal(204);
     })
 
-    describe('delete dialog with correspondence resource without correct scope', () => {
-        let dialogId = dialogIds.pop();
-        let response = purgeSO('dialogs/' + dialogId);
+    describe('deconste dialog with correspondence resource without correct scope', () => {
+        const dialogId = dialogIds.pop();
+        const response = purgeSO('dialogs/' + dialogId);
         expectStatusFor(response).to.equal(403);
         expect(response, 'response').to.have.validJsonBody();
         expect(response.json(), 'response body').to.have.property('errors');
         dialogIds.push(dialogId);
     })
 
-    describe('delete dialog with correspondence resource with correct scope', () => {
-        let dialogId = dialogIds.pop();
-        let r = purgeSO("dialogs/" + dialogId, null, tokenOptions);
+    describe('deconste dialog with correspondence resource with correct scope', () => {
+        const dialogId = dialogIds.pop();
+        const r = purgeSO("dialogs/" + dialogId, null, tokenOptions);
         expectStatusFor(r).to.equal(204)
     })
 
