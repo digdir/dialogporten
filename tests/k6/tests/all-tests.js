082445f8e860733281d187da16c5bdb02c585378
Ole Jørgen Skogstad <skogstad@softis.net>
2024-09-19 16:38:30 +0200

    rv

--- tests/k6/tests/all-tests.js
+++ tests/k6/tests/all-tests.js
@@ -1,7 +1,7 @@
-import { default as enduserTests } from './enduser/all-tests.js';
 import { default as serviceOwnerTests } from './serviceowner/all-tests.js';
+import { default as enduserTests } from './enduser/all-tests.js';
 
 export function runAllTests() {
-    enduserTests();
     serviceOwnerTests();
+    enduserTests();
 };
