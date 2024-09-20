162ce9a9a0c4183d10e8edfe0f8c5589110b7a59
Ole Jørgen Skogstad <skogstad@softis.net>
2024-09-20 11:39:57 +0200

    fix: Add missing events to dialog subscription (#1163)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    The DialogUpdated GraphQL subscription is not triggered when an activity
    is added, or when the dialog is deleted
    
    * Rename subscription to "dialogEvents"
    * Add type field, values are "DialogUpdated" and "DialogDeleted"
    
    ## Related Issue(s)
    
    - #1162
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)
    
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    - **New Features**
    - Introduced a new `DialogEventPayload` structure to handle dialog
    events, including updates and deletions.
    	- Updated subscriptions to reflect the new event handling mechanism.
    
    - **Bug Fixes**
    - Renamed and expanded dialog event handling to improve clarity and
    functionality.
    
    - **Chores**
    - Renamed constants for event topics to better represent the scope of
    dialog events.
    
    - **Tests**
    - Minor adjustments in architecture tests for internal class visibility.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- src/Digdir.Domain.Dialogporten.Infrastructure/GraphQL/GraphQlSubscriptionConstants.cs
+++ src/Digdir.Domain.Dialogporten.Infrastructure/GraphQL/GraphQlSubscriptionConstants.cs
@@ -3,5 +3,5 @@ namespace Digdir.Domain.Dialogporten.Infrastructure.GraphQl;
 public static class GraphQlSubscriptionConstants
 {
     public const string SubscriptionTopicPrefix = "graphql_subscriptions_";
-    public const string DialogUpdatedTopic = "dialogUpdated/";
+    public const string DialogEventsTopic = "dialogEvents/";
 }
