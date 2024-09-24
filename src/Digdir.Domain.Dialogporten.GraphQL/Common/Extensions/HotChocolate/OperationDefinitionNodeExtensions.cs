03dc1f411c95b321ca037182b68e3fd191064a1a
Ole Jørgen Skogstad <skogstad@softis.net>
2024-09-24 23:01:48 +0200

    Add XML comment

--- src/Digdir.Domain.Dialogporten.GraphQL/Common/Extensions/HotChocolate/OperationDefinitionNodeExtensions.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/Common/Extensions/HotChocolate/OperationDefinitionNodeExtensions.cs
@@ -8,6 +8,12 @@ public static class OperationDefinitionNodeExtensions
     private const string DialogIdArgumentName = "dialogId";
     private const string DialogEventsFieldName = nameof(Subscriptions.DialogEvents);
 
+    /// <summary>
+    /// Attempts to extract the dialog ID from a DialogEvents subscription operation.
+    /// </summary>
+    /// <param name="definition">The GraphQL operation definition node.</param>
+    /// <param name="dialogId">When this method returns, contains the extracted dialog ID if found; otherwise, Guid.Empty.</param>
+    /// <returns>True if the dialog ID was successfully extracted; otherwise, false.</returns>
     public static bool TryGetDialogEventsSubscriptionDialogId(this OperationDefinitionNode definition, out Guid dialogId)
     {
         dialogId = Guid.Empty;
