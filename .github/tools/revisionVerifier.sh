c9f23f56b14398345f345582b143bfd9679a18b4
Ole Jørgen Skogstad <skogstad@softis.net>
2024-09-30 11:53:18 +0200

    fix?

--- .github/tools/revisionVerifier.sh
+++ .github/tools/revisionVerifier.sh
@@ -1,3 +1,10 @@
+if ! command -v jq 2>&1 >/dev/null
+then
+    echo "jq could not be found, attempting to install from tdnf ..."
+    tdnf install -y jq
+fi
+
+
 if [ -z "$1" ]; then
   echo "Usage: $0 <revision-name>"
   exit 1
@@ -14,10 +21,11 @@ query_filter="{name:name, runningState:properties.runningState, healthState:prop
 
 verify_revision() {
   local json_output
-  
+
   # Fetch app revision
   json_output=$(az containerapp revision show -g "$resource_group" --revision "$revision_name" --query "$query_filter" 2>/dev/null)
-  
+
+
   health_state=$(echo $json_output | jq -r '.healthState')
   running_state=$(echo $json_output | jq -r '.runningState')
 
@@ -26,7 +34,7 @@ verify_revision() {
   echo "Health state: $health_state"
   echo "Running state: $running_state"
   echo " "
-  
+
   # Check health and running status
   if [[ $health_state == "Healthy" && ($running_state == "Running" || $running_state == "RunningAtMaxScale") ]]; then
     return 0  # OK!
