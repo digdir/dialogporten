8a3b57db8e2c705bfa6e4748d2e7a298249b8259
Magnus Sandgren <5285192+MagnusSandgren@users.noreply.github.com>
2024-09-30 15:52:17 +0200

    Remove outbox scheduler from architecture tests.

--- tests/Digdir.Domain.Dialogporten.Architecture.Tests/InfrastructureArchitectureTests.cs
+++ tests/Digdir.Domain.Dialogporten.Architecture.Tests/InfrastructureArchitectureTests.cs
@@ -1,6 +1,5 @@
 using Digdir.Domain.Dialogporten.Infrastructure;
 using Digdir.Domain.Dialogporten.Infrastructure.Common.Exceptions;
-using Digdir.Domain.Dialogporten.Infrastructure.DomainEvents.Outbox.Dispatcher;
 using FluentAssertions;
 using NetArchTest.Rules;
 
@@ -17,7 +16,6 @@ public class InfrastructureArchitectureTests
             nameof(InfrastructureExtensions),
 
             // These classes are currently public but should be internal, moved to another assembly, or deleted
-            nameof(OutboxScheduler),
             nameof(IUpstreamServiceError)
         };
 
