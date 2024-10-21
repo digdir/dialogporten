6c859ea6859cd8d0584be97cd1ece25cfd83c49b
Dagfinn Olsen <dagfinno@gmail.com>
2024-10-21 14:30:42 +0200

    test(load-tests): add load test for simple search (enduser)

---
+++ tests/k6/tests/enduser/performance/simple-search.js
@@ -0,0 +1,43 @@
+import { getEU, expect, expectStatusFor, describe, getEnduserTokenFromGenerator } from "../../../common/testimports.js";
+import { SharedArray } from 'k6/data';
+import papaparse from 'https://jslib.k6.io/papaparse/5.1.1/index.js';
+
+const filenameEndusers = '../../performancetest_data/.endusers-with-tokens.csv';
+
+const endUsers = new SharedArray('endUsers', function () {
+    return papaparse.parse(open(filenameEndusers), { header: true, skipEmptyLines: true }).data;
+  });
+
+export let options = {
+    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
+    thresholds: {
+        'http_req_duration{name:simple search}': [],
+        'http_reqs{name:simple search}': [],
+    },
+};
+
+export default function() {
+    if ((options.vus === undefined || options.vus === 1) && (options.iterations === undefined || options.iterations === 1)) {
+        simpleSearch(endUsers[0]);
+    }
+    else {
+        while (true) { simpleSearch(randomItem(endUsers)); }
+    }
+}
+
+export function simpleSearch(enduser) {
+    let paramsWithToken = {
+        Headers: {
+            Authorization: "Bearer " + enduser.token
+        },
+        tags: { name: 'simple search' }
+    }
+    let defaultParty = "urn:altinn:person:identifier-no:" + enduser.ssn;
+    let defaultFilter = "?Party=" + defaultParty;
+    describe('Perform simple dialog list', () => {
+        let r = getEU('dialogs' + defaultFilter, paramsWithToken);
+        expectStatusFor(r).to.equal(200);
+        expect(r, 'response').to.have.validJsonBody();
+    });
+}
+
