62598aeb41a02e7b662320a582a24cc624c59537
Ole Jørgen Skogstad <skogstad@softis.net>
2024-11-03 22:48:15 +0100

    brp

--- tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/NotificationCondition/NotificationConditionTests.cs
+++ tests/Digdir.Domain.Dialogporten.Application.Integration.Tests/Features/V1/ServiceOwner/NotificationCondition/NotificationConditionTests.cs
@@ -1,8 +1,6 @@
-using Digdir.Domain.Dialogporten.Application.Features.V1.Common.Localizations;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.DialogActivities.Queries.NotificationCondition;
 using Digdir.Domain.Dialogporten.Application.Features.V1.ServiceOwner.Dialogs.Commands.Create;
 using Digdir.Domain.Dialogporten.Application.Integration.Tests.Common;
-using Digdir.Domain.Dialogporten.Domain.Actors;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Activities;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities.Transmissions;
 using Digdir.Tool.Dialogporten.GenerateFakeData;
@@ -21,7 +19,7 @@ public class NotificationConditionTests(DialogApplication application) : Applica
 
     [Theory]
     [MemberData(nameof(NotificationConditionTestData))]
-    public async Task NotificationCondition_Should_Return_True_For_SendNotification_When_Condition_Is_Met(
+    public async Task SendNotification_Should_Be_True_When_Conditions_Are_Met(
         DialogActivityType.Values activityType,
         NotificationConditionType conditionType)
     {
@@ -61,26 +59,13 @@ public class NotificationConditionTests(DialogApplication application) : Applica
         CreateDialogCommand createDialogCommand,
         DialogActivityType.Values activityType)
     {
-        createDialogCommand.Activities.Add(new ActivityDto
-        {
-            Type = activityType,
-            PerformedBy = new ActivityPerformedByActorDto
-            {
-                ActorType = ActorType.Values.ServiceOwner
-            }
-        });
+        var activity = DialogGenerator.GenerateFakeDialogActivity(type: activityType);
+        createDialogCommand.Activities.Add(activity);
 
-        if (activityType is DialogActivityType.Values.TransmissionOpened)
-        {
-            var transmission =
-                DialogGenerator.GenerateFakeDialogTransmissions(type: DialogTransmissionType.Values.Information)[0];
-            createDialogCommand.Transmissions.Add(transmission);
-            createDialogCommand.Activities[0].TransmissionId = createDialogCommand.Transmissions[0].Id;
-        }
-        else if (activityType is DialogActivityType.Values.Information)
-        {
-            createDialogCommand.Activities[0].Description =
-                [new LocalizationDto { LanguageCode = "nb", Value = "Info" }];
-        }
+        if (activityType is not DialogActivityType.Values.TransmissionOpened) return;
+
+        var transmission = DialogGenerator.GenerateFakeDialogTransmissions(type: DialogTransmissionType.Values.Information)[0];
+        createDialogCommand.Transmissions.Add(transmission);
+        createDialogCommand.Activities[0].TransmissionId = createDialogCommand.Transmissions[0].Id;
     }
 }
