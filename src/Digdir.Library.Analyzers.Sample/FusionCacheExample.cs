fd09cfea43d5a28eab5bedd253b45c88adce44af
Ole Jørgen Skogstad <skogstad@softis.net>
2024-11-28 14:45:32 +0100

    debug?

---
+++ src/Digdir.Library.Analyzers.Sample/FusionCacheExample.cs
@@ -0,0 +1,23 @@
+﻿using ZiggyCreatures.Caching.Fusion;
+
+namespace Digdir.Library.Analyzers.Sample;
+
+public sealed class FusionCacheExample(IFusionCache fusionCache)
+{
+    public async Task<TheCachedThing> GetCachedValue(CancellationToken cancellationToken)
+    {
+        var authorizedParties = await fusionCache.GetOrSetAsync<TheCachedThing>("Some key",
+            GetValueToPutInCache, token: cancellationToken);
+
+        return authorizedParties;
+    }
+
+
+    private static async Task<TheCachedThing> GetValueToPutInCache(CancellationToken token)
+    {
+        await Task.Delay(0, token);
+        return new TheCachedThing();
+    }
+}
+
+public readonly struct TheCachedThing;
