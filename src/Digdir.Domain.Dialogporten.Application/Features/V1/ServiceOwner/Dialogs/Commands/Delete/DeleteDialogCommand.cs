f14861dd72e4bea41e8b8d9e2914966b1ba3f828
Ole Jørgen Skogstad <skogstad@softis.net>
2024-11-29 14:34:08 +0100

    fix(webapi): Repeat delete requests should return 400 BAD REQUEST (#1542)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #1541 
    
    ## Verification
    
    - [x] **Your** code builds clean without any errors or warnings
    - [x] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)

--- src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Delete/DeleteDialogCommand.cs
+++ src/Digdir.Domain.Dialogporten.Application/Features/V1/ServiceOwner/Dialogs/Commands/Delete/DeleteDialogCommand.cs
@@ -19,7 +19,7 @@ public sealed class DeleteDialogCommand : IRequest<DeleteDialogResult>
 }
 
 [GenerateOneOf]
-public sealed partial class DeleteDialogResult : OneOfBase<Success, EntityNotFound, Forbidden, ConcurrencyError>;
+public sealed partial class DeleteDialogResult : OneOfBase<Success, EntityNotFound, BadRequest, Forbidden, ConcurrencyError>;
 
 internal sealed class DeleteDialogCommandHandler : IRequestHandler<DeleteDialogCommand, DeleteDialogResult>
 {
@@ -44,11 +44,19 @@ internal sealed class DeleteDialogCommandHandler : IRequestHandler<DeleteDialogC
         var dialog = await _db.Dialogs
             .Include(x => x.Activities)
             .WhereIf(!_userResourceRegistry.IsCurrentUserServiceOwnerAdmin(), x => resourceIds.Contains(x.ServiceResource))
+            .IgnoreQueryFilters()
             .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
 
         if (dialog is null)
         {
             return new EntityNotFound<DialogEntity>(request.Id);
+        }
+
+        if (dialog.Deleted)
+        {
+            // TODO: https://github.com/digdir/dialogporten/issues/1543
+            // When restoration is implemented, add a hint to the error message.
+            return new BadRequest($"Entity '{nameof(DialogEntity)}' with key '{request.Id}' has already been removed and cannot be deleted again.");
         }
 
         if (!_userResourceRegistry.UserCanModifyResourceType(dialog.ServiceResourceType))
