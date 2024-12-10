2925d4259166e215f42c12ee6178027ce190c768
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-12-10 15:32:42 +0100

    Collapse subject resource mappings before building sql query

--- src/Digdir.Domain.Dialogporten.Application/Common/Extensions/DbSetExtensions.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/Extensions/DbSetExtensions.cs
@@ -2,7 +2,6 @@ using System.Globalization;
 using System.Text;
 using Digdir.Domain.Dialogporten.Application.Externals.AltinnAuthorization;
 using Digdir.Domain.Dialogporten.Domain.Dialogs.Entities;
-using Digdir.Domain.Dialogporten.Domain.SubjectResources;
 using Microsoft.EntityFrameworkCore;
 
 namespace Digdir.Domain.Dialogporten.Application.Common.Extensions;
@@ -33,23 +32,6 @@ public static class DbSetExtensions
                  """);
             parameters.Add(party);
             parameters.Add(resources);
-        }
-
-        foreach (var (party, subjects) in authorizedResources.SubjectsByParties)
-        {
-            // lang=sql
-            sb.AppendLine(CultureInfo.InvariantCulture, $"""
-                 OR (
-                    "{nameof(DialogEntity.Party)}" = @p{parameters.Count} 
-                    AND "{nameof(DialogEntity.ServiceResource)}" = ANY(
-                        SELECT "{nameof(SubjectResource.Resource)}" 
-                        FROM "{nameof(SubjectResource)}" 
-                        WHERE "{nameof(SubjectResource.Subject)}" = ANY(@p{parameters.Count + 1})
-                    )
-                 )
-                 """);
-            parameters.Add(party);
-            parameters.Add(subjects);
         }
 
         return dialogs.FromSqlRaw(sb.ToString(), parameters.ToArray());
