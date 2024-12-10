5fca0b938924fc25e051fb3d53ad3ff1a7e876e2
Bjørn Dybvik Langfors <bdl@digdir.no>
2024-12-10 18:00:30 +0100

    Group parties with identical resource sets

--- src/Digdir.Domain.Dialogporten.Application/Common/Extensions/DbSetExtensions.cs
+++ src/Digdir.Domain.Dialogporten.Application/Common/Extensions/DbSetExtensions.cs
@@ -11,7 +11,6 @@ public static class DbSetExtensions
     public static IQueryable<DialogEntity> PrefilterAuthorizedDialogs(this DbSet<DialogEntity> dialogs, DialogSearchAuthorizationResult authorizedResources)
     {
         var parameters = new List<object>();
-
         // lang=sql
         var sb = new StringBuilder()
             .AppendLine(CultureInfo.InvariantCulture, $"""
@@ -21,19 +20,43 @@ public static class DbSetExtensions
                 """);
         parameters.Add(authorizedResources.DialogIds);
 
-        foreach (var (party, resources) in authorizedResources.ResourcesByParties)
+        // Group parties that have the same resources
+        var groupedResult = authorizedResources.ResourcesByParties
+            .GroupBy(kv => kv.Value, new HashSetEqualityComparer<string>())
+            .ToDictionary(
+                g => g.Key, // The shared HashSet<string>
+                g => new HashSet<string>(g.Select(kv => kv.Key)) // Aggregate all keys with this HashSet<string>
+            );
+
+        foreach (var (resources, parties) in groupedResult)
         {
-            // lang=sql
             sb.AppendLine(CultureInfo.InvariantCulture, $"""
                  OR (
-                    "{nameof(DialogEntity.Party)}" = @p{parameters.Count} 
+                    "{nameof(DialogEntity.Party)}" = ANY(@p{parameters.Count}) 
                     AND "{nameof(DialogEntity.ServiceResource)}" = ANY(@p{parameters.Count + 1})
                  )
                  """);
-            parameters.Add(party);
+            parameters.Add(parties);
             parameters.Add(resources);
         }
 
         return dialogs.FromSqlRaw(sb.ToString(), parameters.ToArray());
+    }
+}
+
+public class HashSetEqualityComparer<T> : IEqualityComparer<HashSet<T>>
+{
+    public bool Equals(HashSet<T>? x, HashSet<T>? y)
+    {
+        return ReferenceEquals(x, y) || (x is not null && y is not null && x.SetEquals(y));
+    }
+
+    public int GetHashCode(HashSet<T> obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+        unchecked
+        {
+            return obj.Aggregate(0, (hash, item) => hash ^ (item?.GetHashCode() ?? 0));
+        }
     }
 }
