46fa464fdfc00220320a75cb317444d8140414e6
Dagfinn Olsen <dagfinno@gmail.com>
2024-12-10 14:58:24 +0100

    a testversion for running multiple k6 tests

--- tests/k6/tests/enduser/enduserSearchWithThresholds.js
+++ tests/k6/tests/enduser/enduserSearchWithThresholds.js
@@ -3,7 +3,7 @@ import { default as run } from "./performance/enduser-search.js";
 export let options = {
     summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
     vus: 1,
-    duration: "30m",
+    duration: "30s",
     thresholds: {
         "http_req_duration{name:get dialog}": ["p(95)<100", "p(99)<300"],
         "http_req_duration{name:get dialog activities}": ["p(95)<100", "p(99)<300"],
