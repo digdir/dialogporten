d49c394309133b0229edcfbad816d9d4a853427f
Ole Jørgen Skogstad <skogstad@softis.net>
2024-12-21 16:09:58 +0100

    brp

--- src/Digdir.Tool.Dialogporten.LargeDataSetGenerator/Dialog.cs
+++ src/Digdir.Tool.Dialogporten.LargeDataSetGenerator/Dialog.cs
@@ -4,14 +4,19 @@ namespace Digdir.Tool.Dialogporten.LargeDataSetGenerator;
 internal static class Dialog
 {
     private static readonly string[] ServiceResources = File.ReadAllLines("./service_resources");
+    private static readonly string[] Parties = File.ReadAllLines("./parties");
+
     public const string CopyCommand = """COPY "Dialog" ("Id", "CreatedAt", "Deleted", "DeletedAt", "DueAt", "ExpiresAt", "ExtendedStatus", "ExternalReference", "Org", "Party", "PrecedingProcess", "Process", "Progress", "Revision", "ServiceResource", "ServiceResourceType", "StatusId", "VisibleFrom", "UpdatedAt") FROM STDIN (FORMAT csv, HEADER false, NULL '')""";
 
     public static string Generate(DialogTimestamp dto)
     {
         var serviceResourceIndex = dto.Counter % ServiceResources.Length;
         var serviceResource = ServiceResources[serviceResourceIndex];
-        // TODO: Party round robin
+
+        var partyIndex = dto.Counter % Parties.Length;
+        var party = Parties[partyIndex];
+
         return
-            $"{dto.DialogId},{dto.FormattedTimestamp},FALSE,,,,sql-generated,NULL,ttd,partyHere,NULL,NULL,11,{Guid.NewGuid()},{serviceResource},GenericAccessResource,1,,{dto.FormattedTimestamp}";
+            $"{dto.DialogId},{dto.FormattedTimestamp},FALSE,,,,sql-generated,NULL,ttd,{party},NULL,NULL,11,{Guid.NewGuid()},{serviceResource},GenericAccessResource,1,,{dto.FormattedTimestamp}";
     }
 }
