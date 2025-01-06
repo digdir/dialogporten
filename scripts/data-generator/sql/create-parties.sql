dd8c48475d904f22e0684bb13b1912d9f9e00d54
Ole Jørgen Skogstad <skogstad@softis.net>
2025-01-06 10:21:08 +0100

    --wip-- [skip ci]

--- scripts/data-generator/sql/create-parties.sql
+++
@@ -1,8 +0,0 @@
--- This table will be populated by COPY command in the setup script.
--- Requires a file named 'parties.txt' in the data-generator directory.
--- This is git-ignored due to its size.
-CREATE TABLE temp_parties
-(
-    Id SERIAL PRIMARY KEY,
-    value TEXT
-);
