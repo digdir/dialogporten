ff335b773d0d9700572a4e7a916a8bc5eaf2f0ad
Ole Jørgen Skogstad <skogstad@softis.net>
2025-01-13 13:50:34 +0100

    --wip-- [skip ci]

--- src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialogs/ObjectTypes.cs
+++ src/Digdir.Domain.Dialogporten.GraphQL/EndUser/SearchDialogs/ObjectTypes.cs
@@ -37,7 +37,7 @@ public sealed class SearchDialogsPayload
     public List<ISearchDialogError> Errors { get; set; } = [];
 }
 
-[GraphQLDescription("If more than one property is set, is bad.")]
+[GraphQLDescription("Set only one property per object. If more than one property is set, there is no guarantee which one will be used.")]
 public sealed class SearchDialogSortType
 {
     public SortDirection? CreatedAt { get; set; }
