fe93b207402738ebc3982f2bcece9f184adc09db
Dagfinn Olsen <dagfinno@gmail.com>
2025-02-04 14:53:41 +0100

    chore(performance): Adding breakpoint tests (#1793)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    This PR adds breakpoint tests, where you give target number of vus,
    duration of test and option to abort the test of failures.
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue(s)
    
    - #1792 
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Ole Jørgen Skogstad <skogstad@softis.net>
    Co-authored-by: Are Almaas <arealmaas@gmail.com>

---
+++ tests/k6/tests/serviceowner/performance/createDialogBreakpoint.js
@@ -0,0 +1,24 @@
+import { createDialog } from '../../performancetest_common/createDialog.js'
+import { endUsers, serviceOwners } from '../../performancetest_common/readTestdata.js';
+import { randomItem } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+
+const traceCalls = (__ENV.traceCalls ?? 'false') === 'true';
+const stages_duration = (__ENV.stages_duration ?? '1m');
+const stages_target = (__ENV.stages_target ?? '5');
+const abort_on_fail = (__ENV.abort_on_fail ?? 'true') === 'true';
+
+
+export let options = {
+    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
+    thresholds: {
+        "http_req_duration": [{ threshold: "max<10000", abortOnFail: abort_on_fail }], 
+    },
+    executor: 'ramping-arrival-rate', //Assure load increase if the system slows
+    stages: [
+        { duration: stages_duration, target: stages_target }, // simulate ramp-up of traffic from 1 to stages_target users over stages_duration
+    ],
+}
+
+export default function() {
+    createDialog(randomItem(serviceOwners), randomItem(endUsers), traceCalls); 
+}
