8022650bfa409b6edf9f87353cf5e587876d4373
Dagfinn Olsen <dagfinno@gmail.com>
2025-02-12 13:53:15 +0100

    run longer test with more testdata

--- tests/k6/tests/enduser/performance/enduserSearchWithThresholds.js
+++ tests/k6/tests/enduser/performance/enduserSearchWithThresholds.js
@@ -1,13 +1,13 @@
 import { default as run, options as _options, setup as _setup } from "./enduser-search.js";
 
-const numberOfEndUsers = 100; // Remove when altinn-testtools bulk get of endusers/tokens is fast
+const numberOfEndUsers = 2000; // Remove when altinn-testtools bulk get of endusers/tokens is fast
 export let options = {
     summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
     vus: 1,
     duration: "30s",
     thresholds: {
         ..._options.thresholds,
-        "http_req_duration{name:enduser search}": ["p(95)<30"],
+        "http_req_duration{name:enduser search}": ["p(95)<300"],
         "http_req_duration{name:get dialog}": ["p(95)<300"],
         "http_req_duration{name:get dialog activities}": ["p(95)<300"],
         "http_req_duration{name:get dialog activity}": ["p(95)<300"],
