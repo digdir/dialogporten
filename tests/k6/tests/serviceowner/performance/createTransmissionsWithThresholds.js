3e6e3b9db45f6e94cbebc6363621420c7e83ad5d
Dagfinn Olsen <dagfinno@gmail.com>
2025-02-13 10:39:35 +0100

    chore(performance): Adjust thresholds (#1870)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Adjust smoketests thresholds for p95 and removed thresholds for p99.
    Use k8s k6 operator cluster instead of github runner
    
    ## Related Issue(s)
    
    - #1862 
    
    ## Verification
    
    - [ ] **Your** code builds clean without any errors or warnings
    - [ ] Manual testing done (required)
    - [ ] Relevant automated test added (if you find this hard, leave it and
    we'll help out)
    
    ## Documentation
    
    - [ ] Documentation is updated (either in `docs`-directory, Altinnpedia
    or a separate linked PR in
    [altinn-studio-docs.](https://github.com/Altinn/altinn-studio-docs), if
    applicable)

---
+++ tests/k6/tests/serviceowner/performance/createTransmissionsWithThresholds.js
@@ -0,0 +1,25 @@
+import { randomItem } from '../../../common/k6-utils.js';
+import { createTransmissions } from '../../performancetest_common/createDialog.js';
+import { serviceOwners, endUsers } from '../../performancetest_common/readTestdata.js';
+
+export const options = {
+    summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(95)', 'p(99)', 'p(99.5)', 'p(99.9)', 'count'],
+    vus: 1,
+    duration: "30s",
+    thresholds: {
+        "http_req_duration{name:create dialog}": ["p(95)<200"],
+        "http_reqs{name:create dialog}": [],
+        "http_req_duration{name:create transmission}": ["p(95)<200"],
+        "http_reqs{name:create transmission}": []
+    }
+};
+
+const traceCalls = (__ENV.traceCalls ?? 'false') === 'true';
+const numberOfTransmissions = (__ENV.numberOfTransmissions ?? '10');
+const maxTransmissionsInThread = (__ENV.maxTransmissionsInThread ?? '100');
+const testid = (__ENV.TESTID ?? 'createTransmissions');
+  
+export default function() {
+    createTransmissions(serviceOwners[0], randomItem(endUsers), traceCalls, numberOfTransmissions, maxTransmissionsInThread, testid);
+}
+
