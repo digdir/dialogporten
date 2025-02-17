ca24e9d83fec4684e7e19c75d2388940be47a4e2
Dagfinn Olsen <dagfinno@gmail.com>
2025-02-17 08:27:37 +0100

    add number of end users as environment variable

--- tests/k6/tests/scripts/run-test-in-k8s.sh
+++ tests/k6/tests/scripts/run-test-in-k8s.sh
@@ -175,7 +175,7 @@ EOF
 kubectl apply -f config.yml
 
 # Wait for the job to finish
-wait_timeout="${duration}100s"
+wait_timeout="${duration}200s"
 kubectl wait --for=jsonpath='{.status.stage}'=finished testrun/$name --timeout=$wait_timeout
 # Print the logs of the pods
 print_logs
